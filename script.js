66d3a56030fcd7a5ff9d14f72275e886d31eac2e
triloon <dimontopcik59@gmail.com>
2025-08-18 08:09:56 +0300

    finish

--- script.js
+++ script.js
@@ -29,6 +29,12 @@
 
 
 import headerHtml from "bundle-text:./fragment/header.html";
+import heroHtml from "bundle-text:./heroS.html";
+import fleetHtml from "bundle-text:./fragment/fleetD.html"
+import yachtsHtml from "bundle-text:./yachtsS.html"
+import activitiesHtml from "bundle-text:./activitiesD.html"
+import bookingHtml from "bundle-text:./bookingT.html"
+import reviewsHtml from "bundle-text:./reviewsT.html"
 import fleetHtml from "bundle-text:./fragment/fleetD.html"
 console.log('Тип headerHtml:', typeof headerHtml);
 console.log('Значення headerHtml:', headerHtml);
@@ -44,5 +50,11 @@ function insertHtmlFragment(targetId, htmlContent) {
 
 document.addEventListener('DOMContentLoaded', () => {
     insertHtmlFragment('header-placeholder', headerHtml);
+    insertHtmlFragment('hero-placeholder', heroHtml)
     insertHtmlFragment('fleet-placeholder', fleetHtml)
+    insertHtmlFragment('yachts-placeholder', yachtsHtml)
+    insertHtmlFragment('activities-placeholder', activitiesHtml)
+    insertHtmlFragment('booking-placeholder', bookingHtml)
+    insertHtmlFragment('reviews-placeholder', reviewsHtml)
+    insertHtmlFragment('footer-placeholder', footerHtml)
 });
